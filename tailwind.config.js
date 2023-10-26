f656861b2bf5d34d1dee143084de4ecf01089c35
Dogecat0 <zcmmwang@gmail.com>
2023-10-26 18:47:28 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ['./themes/my_theme/layout/**/*.ejs'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
