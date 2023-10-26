696e49bc31d6d2072ab2c755cf5d347c5e288444
Dogecat0 <zcmmwang@gmail.com>
2023-10-27 00:24:08 +0100

    feat: Add css colours and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   mode: 'jit',
   content: ['./themes/my_theme/layout/**/*.ejs'],
   theme: {
-    extend: {},
+    colors: {
+      'primary': '#121212',
+      'secondary': '#faf3e0',
+      'tertiary': '#102620',
+      'accent': '#cc3700',
+      'accent-light': '#CD8A50',
+    },
+    extend: {
+      fontFamily: {
+        'pop': ['Poppins', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
